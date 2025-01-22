fc7489a5eeb9467e3bf58d204cb5f229727ea640
MiguelAssis0 <miganjos88@gmail.com>
2025-01-22 19:10:27 -0300

    configurando o cors

--- src/main/java/com/MiguelAnjos/dslist/config/CorsConfig.java
+++ src/main/java/com/MiguelAnjos/dslist/config/CorsConfig.java
@@ -1,17 +1,25 @@
 package com.MiguelAnjos.dslist.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*");
+    @Value("${cors.origins}")
+    private String origin;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedMethods("*")
+                    .allowedOrigins(origin);
+            }
+        };
     }
 }
