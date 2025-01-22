afe7c61f05421cc4e56c67885333955770f14137
MiguelAssis0 <miganjos88@gmail.com>
2025-01-22 18:29:39 -0300

    webMVC

--- src/main/java/com/MiguelAnjos/dslist/DslistApplication.java
+++ src/main/java/com/MiguelAnjos/dslist/DslistApplication.java
@@ -2,8 +2,10 @@ package com.MiguelAnjos.dslist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class DslistApplication {
 
 	public static void main(String[] args) {
